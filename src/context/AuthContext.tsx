385de84970be947bff7be13f0ab48195fbcc5838
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-01-21 17:19:24 +0100

    correction user type

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,7 +4,7 @@ import firebaseService from "../services/firebase";
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState<User>(null)
+    const [currentUser, setCurrentUser] = useState<React.User>(null)
     const [isLoaded, setIsLoaded] = useState<Boolean>(false)
 
     useEffect(() => {
